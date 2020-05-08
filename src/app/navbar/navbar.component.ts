e127ff17cc862bda316a5d3497e70da456729808
unknown <pranay12430.nagavolu@gmail.com>
2020-05-09 01:02:26 +0530

    multiple additions and changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,7 +9,33 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  scroll(element){   
+    const ele = document.getElementById(element);   
+    window.scrollTo(ele.offsetLeft,ele.offsetTop);
+  }
+
+
   ngOnInit(): void {
+    var quotes=[
+      'Your limitation is only your imagination',
+      ' Sometimes later becomes never. Do it now.',
+      'The way to get started is to quit talking and begin doing.',
+      'If you want a thing well done, DO IT YOURSELF.',
+      'If you don\'t start TODAY, then what makes you think you\'ll start TOMORROW',
+    ]
+    var randomnumber = Math.floor(Math.random()*(quotes.length));
+    document.getElementById('displayquote').innerHTML = quotes[randomnumber];
+  }
+  change(){
+    var quotes=[
+      'Your limitation—is only your imagination',
+      ' Sometimes later becomes never. Do it now.',
+      'If you don\'t start TODAY, then what makes you think you\'ll start TOMORROW',
+      'If you want a thing well done, DO IT YOURSELF.',
+      'The way to get started is to quit talking and begin doing.'
+    ]
+    var randomnumber = Math.floor(Math.random()*(quotes.length));
+    document.getElementById('displayquote').innerHTML = quotes[randomnumber];
   }
 
 }
